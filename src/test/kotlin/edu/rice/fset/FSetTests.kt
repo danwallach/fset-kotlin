caaa956dd869d9d3ed37e34442e076bbf2d3b5a2
Dan Wallach <dwallach@rice.edu>
2021-04-28 14:59:04 -0500

    minor cleanup

--- src/test/kotlin/edu/rice/fset/FSetTests.kt
+++ src/test/kotlin/edu/rice/fset/FSetTests.kt
@@ -31,73 +31,72 @@ val listIntWithLameHashGen = arbitrary { rs ->
 
 fun <E : Any> setsEqual(a: Set<E>, b: Set<E>): Boolean = a.containsAll(b) && b.containsAll(a)
 
-fun <E : Any> fsetTests(
+fun fsetTests(
     algorithm: String,
     emptyStringSet: FSet<String>,
     emptyIntSet: FSet<IntWithLameHash>
-) =
-    freeSpec {
-        algorithm - {
-            "basic inserts and membership (string)" {
-                checkAll<List<String>> { inputs ->
-                    val testMe = emptyStringSet.addAll(inputs.asIterable())
-                    inputs.forEach { i ->
-                        // hash collisions are basically never going to happen here
-                        val desiredHash = i.hashCode()
-                        val result = testMe.lookup(desiredHash).asSequence().toSet()
-                        val expected = inputs.filter { it.hashCode() == desiredHash }.toSet()
-                        assertEquals(result, expected)
-                    }
+) = freeSpec {
+    algorithm - {
+        "basic inserts and membership (string)" {
+            checkAll<List<String>> { inputs ->
+                val testMe = emptyStringSet.addAll(inputs.asIterable())
+                inputs.forEach { i ->
+                    // hash collisions are basically never going to happen here
+                    val desiredHash = i.hashCode()
+                    val result = testMe.lookup(desiredHash).asSequence().toSet()
+                    val expected = inputs.filter { it.hashCode() == desiredHash }.toSet()
+                    assertEquals(result, expected)
                 }
             }
-            "basic inserts and membership (restricted int)" {
-                checkAll(listIntWithLameHashGen) { inputs ->
+        }
+        "basic inserts and membership (restricted int)" {
+            checkAll(listIntWithLameHashGen) { inputs ->
+                val testMe = emptyIntSet.addAll(inputs.asIterable())
+                inputs.forEach { i ->
+                    // hash collisions are going to happen all the time here
+                    val desiredHash = i.hashCode()
+                    val result = testMe.lookup(desiredHash).asSequence().toSet()
+                    val expected = inputs.filter { it.hashCode() == desiredHash }.toSet()
+                    assertEquals(result, expected)
+                }
+            }
+        }
+        "queries for missing values (restricted int)" {
+            checkAll(listIntWithLameHashGen, intWithLameHashGen) { inputs, other ->
+                if (!inputs.contains(other)) {
                     val testMe = emptyIntSet.addAll(inputs.asIterable())
-                    inputs.forEach { i ->
-                        // hash collisions are going to happen all the time here
-                        val desiredHash = i.hashCode()
-                        val result = testMe.lookup(desiredHash).asSequence().toSet()
-                        val expected = inputs.filter { it.hashCode() == desiredHash }.toSet()
-                        assertEquals(result, expected)
-                    }
-                }
-            }
-            "queries for missing values (restricted int)" {
-                checkAll(listIntWithLameHashGen, intWithLameHashGen) { inputs, other ->
-                    if (!inputs.contains(other)) {
-                        val testMe = emptyIntSet.addAll(inputs.asIterable())
-                        val results = testMe.lookup(other.hashCode()).asSequence().toList()
-                        results.forEach { i ->
-                            assertEquals(i.hashCode(), other.hashCode())
-                            assertTrue(inputs.contains(i))
-                            assertNotEquals(i, other)
-                        }
-                    }
-                }
-            }
-            "removing a value and set equality (restricted int)" {
-                checkAll(listIntWithLameHashGen, intWithLameHashGen) { inputs, other ->
-                    val testMe = emptyIntSet.addAll(inputs.asIterable())
-                    val testMePlus = testMe + other
-                    val testMinus = testMePlus - other
-
-                    // we're testing that we have the expected set behavior, and we're
-                    // exercising our equals() methods versus the setsEqual() code
-                    // above that uses Kotlin logic from Container.
-                    if (inputs.contains(other)) {
-                        setsEqual(testMe, testMePlus) shouldBe true
-                        testMe shouldBe testMePlus
-                        testMe.hashCode() shouldBe testMePlus.hashCode()
-                        setsEqual(testMe, testMinus) shouldBe false
-                        testMe shouldNotBe testMinus
-                    } else {
-                        setsEqual(testMe, testMinus) shouldBe true
-                        testMe shouldBe testMinus
-                        testMe.hashCode() shouldBe testMinus.hashCode()
-                        setsEqual(testMe, testMePlus) shouldBe false
-                        testMe shouldNotBe testMePlus
+                    val results = testMe.lookup(other.hashCode()).asSequence().toList()
+                    results.forEach { i ->
+                        assertEquals(i.hashCode(), other.hashCode())
+                        assertTrue(inputs.contains(i))
+                        assertNotEquals(i, other)
                     }
                 }
             }
         }
+        "removing a value and set equality (restricted int)" {
+            checkAll(listIntWithLameHashGen, intWithLameHashGen) { inputs, other ->
+                val testMe = emptyIntSet.addAll(inputs.asIterable())
+                val testMePlus = testMe + other
+                val testMinus = testMePlus - other
+
+                // we're testing that we have the expected set behavior, and we're
+                // exercising our equals() methods versus the setsEqual() code
+                // above that uses Kotlin logic from Container.
+                if (inputs.contains(other)) {
+                    setsEqual(testMe, testMePlus) shouldBe true
+                    testMe shouldBe testMePlus
+                    testMe.hashCode() shouldBe testMePlus.hashCode()
+                    setsEqual(testMe, testMinus) shouldBe false
+                    testMe shouldNotBe testMinus
+                } else {
+                    setsEqual(testMe, testMinus) shouldBe true
+                    testMe shouldBe testMinus
+                    testMe.hashCode() shouldBe testMinus.hashCode()
+                    setsEqual(testMe, testMePlus) shouldBe false
+                    testMe shouldNotBe testMePlus
+                }
+            }
+        }
     }
+}
