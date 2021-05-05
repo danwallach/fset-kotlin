410849892720caafdcf9abb2ff8c0258d965d1ee
Dan Wallach <dwallach@rice.edu>
2021-05-05 12:40:19 -0500

    temporary changes to benchmark

--- src/main/kotlin/edu/rice/fset/Benchmark.kt
+++ src/main/kotlin/edu/rice/fset/Benchmark.kt
@@ -19,22 +19,28 @@ internal fun benchmark(name: String, size: Int, offsets: List<Int>, emptyIntSet:
         measure {
             print(".")
             val bigSet1 = emptyIntSet.addAll((offset + 1)..(offset + size))
-//            println("START")
+            println("START")
 //            bigSet1.debugPrint()
+            println(bigSet1.statistics())
             val bigSet2 = bigSet1.removeAll((offset + 1)..(offset + size / 2))
-//            println("REMOVE")
+            println("REMOVE")
 //            bigSet2.debugPrint()
+            println(bigSet2.statistics())
             val bigSet3 = bigSet2.addAll((offset + 1)..(offset + size * 2))
-//            println("ADD")
+            println("ADD")
 //            bigSet3.debugPrint()
+            println(bigSet3.statistics())
             val bigSet4 = bigSet3.removeAll((offset + 1)..(offset + size * 2) step 2)
-//            println("REMOVE AGAIN")
+            println("REMOVE AGAIN")
 //            bigSet4.debugPrint()
 
-            val ignored =
-                ((offset + 1)..(offset + size * 2) step 3).map { bigSet4.lookup(it) ?: 0 }.sum()
+//            val ignored =
+//                ((offset + 1)..(offset + size * 2) step 3).map { bigSet4.lookup(it) ?: 0 }.sum()
 //            bigSet.debugPrint()
-            bigSet4.statistics()
+
+            val stats4 = bigSet4.statistics()
+            println(stats4)
+            stats4
         }
     }.toList()
     println(" Done.")
@@ -50,15 +56,14 @@ internal fun benchmark(name: String, size: Int, offsets: List<Int>, emptyIntSet:
 
 fun main() {
     val size = 40000
-    val iterations = 5
+    val iterations = 1
     val offsets = generateSequence {
         Random.nextInt(Short.MAX_VALUE.toInt())
     }.take(iterations).toList()
-    benchmark("Binary Tree", size, offsets, emptyBinaryTreeSet())
-    benchmark("Binary Choice", size, offsets, emptyBinaryChoiceTreeSet())
-    benchmark("Treap", size, offsets, emptyTreapSet())
-    benchmark("HAMT", size, offsets, emptyHamtSet())
-
-    benchmark("Treap", size * 10, offsets, emptyTreapSet())
+//    benchmark("Binary Tree", size, offsets, emptyBinaryTreeSet())
+//    benchmark("Binary Choice", size, offsets, emptyBinaryChoiceTreeSet())
+//    benchmark("Treap", size, offsets, emptyTreapSet())
+//    benchmark("HAMT", size, offsets, emptyHamtSet())
     benchmark("HAMT", size * 10, offsets, emptyHamtSet())
+//    benchmark("Treap", size * 10, offsets, emptyTreapSet())
 }
